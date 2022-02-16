95ffcb726517f09bfa4ed3ad1ded37c127e90f2c
uzielvivas <62106017+uzielvivas@users.noreply.github.com>
2022-02-16 08:40:26 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp( {
+  data() {
+    return {
+    url: 'http://www.thenetninja.co.uk',
+    showBooks: true,
+    books: [
+      { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+      { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+      { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true},
+    ]
+  }
+},
+methods: {
+  toggleShowBooks() {
+    this.showBooks = !this.ShowBooks
+  },
+  toggleFav(book) {
+    book.isfav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav)
+    }
+  }
+})
+
+app.mount('#app')
